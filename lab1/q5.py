a254b3df5aceb61baf847fe21b14d34e5a566c31
unknown <tonybray86@gmail.com>
2026-07-02 10:50:34 +0100

    feat: created answer for lab 1 question 5

---
+++ lab1/q5.py
@@ -0,0 +1,8 @@
+# Q5 — Einstein (int conversion, `**`)
+
+# Constant - speed of light
+C = 299792458; # m/s
+
+# Conver mass to energe E=MC^2
+mass = float(input("What is the mass in Kg?\n"));
+print (f"{mass} Kg of mass is {mass*C*C} joules");
