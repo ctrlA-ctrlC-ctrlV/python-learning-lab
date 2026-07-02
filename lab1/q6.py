8b9dc29f1131653589b98e1c5db45127dbcd14c4
unknown <tonybray86@gmail.com>
2026-07-02 10:50:44 +0100

    feat: created answer for lab 1 question 6

---
+++ lab1/q6.py
@@ -0,0 +1,7 @@
+# Q6 — how long have you been alive? (arithmetic, thousands formatting)
+
+# Ask user for age
+age_in_years = int(input("What is your age in years?"));
+
+# Covert to date
+print(f"That is about {age_in_years*365:,} days old");
