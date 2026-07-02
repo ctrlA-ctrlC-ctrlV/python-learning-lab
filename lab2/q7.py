839864e8e64ed394cc0966c2c37c4f937e1430e2
unknown <tonybray86@gmail.com>
2026-07-02 16:49:40 +0100

    feat: created answer for lab 2 question 7

---
+++ lab2/q7.py
@@ -0,0 +1,8 @@
+# Q7 — Deep Thought (string ==) 
+
+answer = str(input("The Great Question of Life, the Universe, and Everything.\nWhat is the answer? >> "))
+
+if answer == "42":
+    print("Yes")
+else:
+    print("No")
