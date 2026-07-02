961dc285eaabfd8d1498316c1da6653a6b9fa67a
unknown <tonybray86@gmail.com>
2026-07-02 10:50:54 +0100

    feat: created answer for lab 1 question 7

---
+++ lab1/q7.py
@@ -0,0 +1,9 @@
+# Q7 — spine label (concatenation with `+`, `str()`)
+
+# Ask for Author's surname
+surname = input("Enter author's surname >> ")
+# Ask for year of publication
+year = str(input("Enter year >> "))
+
+# Print spine label
+print(surname + " - " + year);
