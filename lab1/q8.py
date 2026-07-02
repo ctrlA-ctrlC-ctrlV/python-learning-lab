3749c801c703ac0d56171b052688b1b25dd23924
unknown <tonybray86@gmail.com>
2026-07-02 10:51:05 +0100

    feat: created answer for lab 1 question 8

---
+++ lab1/q8.py
@@ -0,0 +1,9 @@
+# Q8 — the `input` trap (input always returns a `str`)
+
+first_num = input("First number >> ");
+second_num = input("Second_num number >> ");
+
+# Glued together
+print(first_num+second_num)
+# Sum
+print(int(first_num)+int(second_num));
