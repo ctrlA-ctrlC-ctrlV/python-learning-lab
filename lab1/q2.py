2607062786b58ff57d85b7ebd3ee8687a1786995
unknown <tonybray86@gmail.com>
2026-07-02 10:50:00 +0100

    feat: created answer for lab 1 question 2

---
+++ lab1/q2.py
@@ -0,0 +1,10 @@
+# **Q2 — a warmer welcome** *(f-strings)*
+
+# msg1 - ask name
+print("What is your name?");
+
+# input - user name
+name = input();
+
+# msg2 - hello, [name]
+print(f"Hello, {name}");
