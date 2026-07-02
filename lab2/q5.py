ba3653c59093117dd6a98c3c938b829e6a7a5e4d
unknown <tonybray86@gmail.com>
2026-07-02 15:49:46 +0100

    feat: created answer for lab 2 question 5

---
+++ lab2/q5.py
@@ -0,0 +1,8 @@
+# Q5 — just right (and)
+
+temp = float(input("Current temperature is >> "))
+
+if temp >= 15 and temp <= 25:
+    print("Just right for reading outside.")
+else:
+    print("Not the best for reading outside.")
