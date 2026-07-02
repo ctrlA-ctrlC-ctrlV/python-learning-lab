90442dd4e2d2e1261ed16617e65ebcf5eb3e0ffe
unknown <tonybray86@gmail.com>
2026-07-02 13:43:05 +0100

    feat: created answer for lab 2 question 2

---
+++ lab2/q2.py
@@ -0,0 +1,8 @@
+# Q2 — odd or even (modulo + if/else)
+
+num = int(input("Enter a integer >> "))
+
+if num % 2 == 0:
+    print(f"The number you input \"{num}\" is even!")
+else:
+    print(f"The number you input \"{num}\" is odd!")
