b10616f950bba82da450cd05f0dfa423a2962acf
unknown <tonybray86@gmail.com>
2026-07-16 12:08:46 +0100

    feat: created answer for lab4 practice 1

---
+++ lab4/practic1.py
@@ -0,0 +1,12 @@
+# Why functions?
+
+print("Hello, Alice!")
+print("Hello, Bob!")
+print("Hello, Carol!")
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
+greet("Bob")
+greet("Carol")
