66c79ab32a2ac8ef25fd8442db9e7e4ed0d02c06
unknown <tonybray86@gmail.com>
2026-07-16 10:11:40 +0100

    feat: updated answer for lab 3 weekly challenge

--- lab3/shelf-v3.py
+++ lab3/shelf-v3.py
@@ -5,7 +5,7 @@ shelf = []  # list for the book
 while 1:
     print("--- Shelf ---")
     
-    user_input = input("[1] Add a book\n[2] List all books\n[3] Search for a book\n[Q] Quit\nChose: ")
+    user_input = input("[1] Add a book\n[2] List all books\n[3] Search for a book\n[4] Remove a book\n[Q] Quit\nChose: ")
 
     if user_input == '1':   # add a book
         title = input("Please enter book title >> ")
@@ -18,9 +18,10 @@ while 1:
         if len(shelf) == 0:
             print("Shelf is empty")
         else:
-            for i in range(len(shelf)):
-                print(f"{i+1}. {shelf[i]}")
-    elif user_input == '3':
+            shelf = sorted(shelf)
+            for index, title in enumerate(shelf, start=1):
+                print(f"{index}. {title}")
+    elif user_input == '3': # search a title
         title = input("Please enter book title you want to search >> ")
         if title in shelf:
             print(f"{title} already on the shelf!")
@@ -29,7 +30,13 @@ while 1:
             if a_input == '1':
                 shelf.append(title)
                 print(f"You have added {title} to the shelf!") 
-
+    elif user_input == '4': # remove a title
+        title = input("Please enter book title you want to remove >> ")
+        if title in shelf:
+            shelf.remove(title)
+            print(f"{title} is removed from the shelf")
+        else:
+            print(f"{title} is not on the shelf!")
     elif user_input.lower() == 'q':
         break
     else:
