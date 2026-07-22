3f5607b1762f36b82bab348c0b3e694979c0d056
unknown <tonybray86@gmail.com>
2026-07-22 12:05:11 +0100

    feat: created answer for part 1.1

---
+++ lab5/practice1.py
@@ -0,0 +1,18 @@
+# 1.1 Why a dictionary?
+
+# Step one
+contact = ['Alice', '555-1234']
+print(contact)
+
+print("======================================================================")
+
+# Step two
+contact = {
+    # key: value
+    "name": "Alice",
+    "phone": "555-1234"
+}
+print(contact)
+print(contact["name"])
+print(contact["phone"])
+
